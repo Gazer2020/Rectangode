4017df152cdb2ebaa469f07199ddb56434a5eb6f
Ke Chen <chen-ke@sjtu.edu.cn>
2023-05-23 22:13:20 +0800

    bitstream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Rectangode import Rectangode
-
+import numpy as np
+from PIL import Image
 s = "My name's Chen Ke. I'm from SJTU."
 r = Rectangode(s)
 r = Rectangode(s, 30, 20)
@@ -7,3 +8,5 @@ r.calculating(k=28)
 r.make()
 
 
+p = np.array(Image.open("output.png").getdata())
+print(p)
