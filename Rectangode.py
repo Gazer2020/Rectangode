37099e4ef67aaec16a0dd86ec67358a12492b01c
Gazer2020 <bicyclegazer@outlook.com>
2023-05-24 18:48:33 +0800

    update correction

--- Rectangode.py
+++ Rectangode.py
@@ -22,7 +22,14 @@ S_POS = np.array([[0, 0, 0, 0, 0, 0],
 
 
 class Rectangode:
-    def __init__(self, _s: str, _r: int = 0, _c: int = 0):
+    def __init__(self):
+        """It create an empty class.
+        You can use create to create an image from string.
+        You can also use open to open an image.
+        """
+        pass
+
+    def create(self, _s: str, _r: int = 0, _c: int = 0):
         """It create a Rectangode class.
 
         Args:
@@ -44,18 +51,25 @@ class Rectangode:
 
         self.k = k
         rs = RSCodec(nsym=self.k)
-        self.code = rs.encode(self.content.encode())
-        self.code = np.array([list(format(byte, '08b'))
-                             for byte in self.code]).astype(int)
-        self.code.resize((self.code.size,))
-        
+
         if self.row and self.col:
-            assert ((self.row*self.col) > (self.code.size + 108))
+            assert ((self.row*self.col) >
+                    (len(self.content)*8 + self.k*8 + 108))
             assert (self.row > 8)
             assert (self.col > 15)
         else:
-            self.row = np.ceil(np.sqrt((self.code.size + 108)/2)).astype(int)
-            self.col = np.ceil((self.code.size + 108) / self.row).astype(int)
+            self.col = np.ceil(
+                np.sqrt((len(self.content)*8 + self.k*8 + 108)*2)).astype(int)
+            self.row = np.ceil(
+                (len(self.content)*8 + self.k*8 + 108) / self.col).astype(int)
+
+        self.code = self.content.encode()
+        self.code = rs.encode(self.code)
+
+        print(self.code)
+        self.code = np.array([list(format(byte, '08b'))
+                             for byte in self.code]).astype(int)
+        self.code.resize((self.code.size,))
 
         # setting a symbol of the error correcting code
         error_code = np.array(
@@ -95,19 +109,48 @@ class Rectangode:
             temp4 = np.concatenate((part3, S_POS), axis=1)
             self.bits = np.concatenate((temp3, temp4), axis=0)
             self.bits.resize((self.row*self.col,))
-            print(self.bits)
 
-    def make(self, output: str = './output.png') -> None:
+    def make_img(self, output: str = './output.png') -> None:
         img = Image.new('1', (self.col, self.row))
         img.putdata(self.bits)
         img.save(output)
-    def open(path):
-        img = Image.open("output.png")
-        bits = np.array(img.getdata())
-        bits[bits==255] = 1
-        size = img.size()
+
+    def open_img(self, path: str = './output.png') -> tuple[np.array, int]:
+        with Image.open(path) as img:
+            self.bits = np.array(img).astype(int)
+
+    def converting(self):
+        print(self.bits)
+        while not np.array_equal(self.bits[:8, :8], L_POS) or not np.array_equal(self.bits[-6:, -6:], S_POS):
+            self.bits = np.rot90(self.bits)
+        error_code = ''.join(self.bits[:8, 8].copy().astype(str))
+
+        print(error_code)
+        self.k = int(error_code, base=2)
+
+        self.row, self.col = self.bits.shape
+        if self.row < 14:
+            part1 = self.bits[:self.row-6, 9:].copy()
+            part2 = self.bits[self.row-6:8, 9:self.col-6].copy()
+            part3 = self.bits[8:, :self.col-6].copy()
+            self.code = ''.join(part1.flatten().astype(
+                str)) + ''.join(part2.flatten().astype(str)) + ''.join(part3.flatten().astype(str))
+        else:
+            part1 = self.bits[:8, 9:].copy()
+            part2 = self.bits[8:self.row-6, :].copy()
+            part3 = self.bits[self.row-6:, :self.col-6].copy()
+            self.code = ''.join(part1.flatten().astype(
+                str)) + ''.join(part2.flatten().astype(str)) + ''.join(part3.flatten().astype(str))
+
+        self.code = bytes([int(self.code[i:i+8], 2)
+                          for i in range(0, len(self.code), 8)]).rstrip(b'\x00')
+        rs = RSCodec(nsym=self.k)
+        print(self.code)
+        return rs.decode(self.code)[0].decode()
+
+
 if __name__ == '__main__':
     s = "I'm Chen Ke."
-    r = Rectangode(s)
-    r.calculating()
-    r.make()
+    r = Rectangode()
+    r.open_img()
+    print(r.converting())
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from Rectangode import Rectangode
-import numpy as np
-from PIL import Image
 s = "My name's Chen Ke. I'm from SJTU."
-r = Rectangode(s)
-r = Rectangode(s, 30, 20)
-r.calculating(k=28)
-r.make()
+r = Rectangode()
+r.create(s)
+r.calculating()
+r.make_img()
+
+# import numpy as np
+# from PIL import Image
+
+# path='./output.png'
+# with Image.open(path) as img:
+#     bits = np.array(img).astype(int)
+# print(bits)
+# bits[-5:, :5] = 1
+# img = Image.new('1', bits.shape[::-1])
+# img.putdata(bits.flatten())
+# img.save(path)
 
